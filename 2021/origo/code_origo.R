b1b0ef17dd9ef54f7ee079c422c13a84e84967dd
jvieroe <jvieroe@gmail.com>
2021-12-12 18:31:03 +0100

    Create code_origo.R

---
+++ 2021/origo/code_origo.R
@@ -0,0 +1,46 @@
+library(tidyverse)
+library(magrittr)
+library(sf)
+library(rnaturalearth)
+library(rnaturalearthhires)
+
+
+origo <- tribble(~lat, ~lon,
+                 0, 0) %>% 
+  st_as_sf(coords = c("lon", "lat"),
+           crs = 4326)
+
+map_sf <- ne_countries(scale = "large") %>% 
+  st_as_sf() %>%
+  #filter(admin == "Cameroon") %>% 
+  select(admin)
+
+grid_sf <- map_sf %>% 
+  st_make_grid(.,
+               n = c(180, 90),
+               square = TRUE) %>% 
+  st_as_sf() %>% 
+  mutate(grid_id = row_number())
+
+grid_sf <- grid_sf %>%  
+  st_intersection(map_sf)
+
+centroids <- grid_sf %>% 
+  st_centroid()
+
+dist <- st_distance(centroids,
+                    origo)
+
+grid_sf <- grid_sf %>% 
+  mutate(dist = dist) %>% 
+  mutate(dist = parse_number(as.character(dist)))
+
+
+ggplot() +
+  geom_sf(data = grid_sf, aes(fill = dist),
+          color = "white", size = .1)
+
+
+
+
+
