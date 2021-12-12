c53ed6535c192cfd4436bc1d6d9b85493a7c7793
jvieroe <jvieroe@gmail.com>
2021-12-12 15:59:24 +0100

    png

--- 2021/plague_british/uk_hyde.R
+++ 2021/plague_british/uk_hyde.R
@@ -174,7 +174,7 @@ ggplot() +
                                         color = NA),
         plot.background = element_rect(fill = "gray15",
                                        color = NA),
-        plot.margin = grid::unit(c(t = 0, r = 30, b = 5, l = 5), "mm")) +
+        plot.margin = grid::unit(c(t = 0, r = 20, b = 5, l = 5), "mm")) +
   guides(fill = guide_colorbar(barwidth = 15,
                                barheight = .5,
                                ticks = FALSE,
@@ -187,8 +187,8 @@ ggplot() +
 
 ggsave(plot = last_plot(),
        filename = "2021/plague_british/black-death_british.png")
-knitr::plot_crop("2021/plague_british/black-death_british.png",
-                 quiet = T)
+# knitr::plot_crop("2021/plague_british/black-death_british.png",
+#                  quiet = T)
 
 ggsave(plot = last_plot(),
        filename = "2021/plague_british/black-death_british.svg")
