a44586318201c0575bf040f87d989acc9d525f48
jvieroe <jvieroe@gmail.com>
2021-12-12 15:57:41 +0100

    png

--- 2021/plague_british/uk_hyde.R
+++ 2021/plague_british/uk_hyde.R
@@ -173,7 +173,8 @@ ggplot() +
         panel.background = element_rect(fill = "gray15",
                                         color = NA),
         plot.background = element_rect(fill = "gray15",
-                                       color = NA)) +
+                                       color = NA),
+        plot.margin = grid::unit(c(t = 0, r = 30, b = 5, l = 5), "mm")) +
   guides(fill = guide_colorbar(barwidth = 15,
                                barheight = .5,
                                ticks = FALSE,
@@ -183,8 +184,6 @@ ggplot() +
                                                           family = "Cinzel",
                                                           size = 7)))
 
-
-# plot.margin = grid::unit(c(t = 0, r = 30, b = 5, l = 5), "mm")
 
 ggsave(plot = last_plot(),
        filename = "2021/plague_british/black-death_british.png")
