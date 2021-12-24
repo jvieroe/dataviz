a60538d16acbe85be06e1332f0aa94bc517670e2
jvieroe <jvieroe@gmail.com>
2021-12-24 16:58:08 +0100

    launch brondby

---
+++ 2021/brondby/code.R
@@ -0,0 +1,52 @@
+library(tidyverse)
+library(readxl)
+library(janitor)
+
+bif <- readxl::read_xlsx("2021/brondby/ws_data.xlsx")
+
+bif <- bif %>% 
+  clean_names()
+
+bif <- bif %>% 
+  slice(-c(1:2))
+
+bif <- bif %>% 
+  filter(competition == "Denmark. Superliga") %>% 
+  select(c(date, match, competition, team, goals, x_g)) %>% 
+  mutate(team = ifelse(team == "Brøndby",
+                       team,
+                       "Opponent"))
+
+bif <- bif %>% 
+  pivot_wider(id_cols = c(date, match, competition),
+              names_from = team,
+              values_from = c(goals,
+                              x_g))
+
+bif <- bif %>% 
+  mutate(result = case_when(goals_Brøndby > goals_Opponent ~ "Win",
+                            goals_Brøndby == goals_Opponent ~ "Draw",
+                            goals_Brøndby < goals_Opponent ~ "Loss"))
+
+
+bif <- bif %>% 
+  mutate(xg_diff = x_g_Brøndby - x_g_Opponent) %>% 
+  mutate(xg_diff_grp = case_when(x_g_Brøndby > x_g_Opponent ~ "Positive",
+                                 x_g_Brøndby == x_g_Opponent ~ "Zero",
+                                 x_g_Brøndby < x_g_Opponent ~ "Negative"))
+
+ggplot(bif) +
+  geom_bar(aes(x = date, y = x_g_Brøndby),
+           stat = "identity",
+           fill = NA,
+           color = "black",
+           size = .1) +
+  # geom_bar(aes(x = date, y = x_g_Opponent),
+  #          stat = "identity",
+  #          alpha = 0.3) +
+  geom_bar(aes(x = date, y = xg_diff, fill = result),
+           stat = "identity",
+           alpha = 0.3,
+           color = "black",
+           size = .1) +
+  theme_minimal()
