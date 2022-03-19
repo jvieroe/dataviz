a7c101ddefe405aaad02e77e34e8145a5c867e50
jvieroe <jvieroe@gmail.com>
2022-03-19 19:07:59 +0100

    Update code_whogov.R

--- 2022/whogov/code_whogov.R
+++ 2022/whogov/code_whogov.R
@@ -38,53 +38,78 @@ url <- "https://api.dataforsyningen.dk/supplerendebynavne2"
 cities <- httr::GET(url) %>% 
   content(.)
 
-# byer <- content(byer)
-# 
-# b1 <- byer[[1]]
-# 
-# b1$navn
-# b1$kommune$kode
-# b1$kommune$navn
-# b1$postnumre
-# 
-# zips <- b1$postnumre[[1]]
-# zips$nr
-# zips$navn
-# 
-# rm(coordss)
-# coordss <- b1$visueltcenter[[1]]
-# coordss
+b1 <- cities[[1]]
 
-extract_func <- function(input, name, municipality, temp, zips, cooords, df) {
+
+b1$navn
+b1$kommune$kode
+b1$kommune$navn
+b1$postnumre
+
+
+zips <- b1$postnumre
+zips
+
+zips <- b1$postnumre[[1]]
+zips
+length(b1$postnumre)
+
+zips$nr
+zips$navn
+
+rm(coords)
+coords <- b1$visueltcenter[[1]]
+coords
+
+rm(b1, zips,
+   coords)
+
+extract_func <- function(input, name, municipality, cooords, df) {
   
   name <- input$navn
   
   municipality <- tibble(muni_code = input$kommune$kode,
                          muni_name = input$kommune$navn)
   
-  temp <- input$postnumre[[1]]
-  
-  zips <- tibble(zip_code = temp$nr,
-                 zip_name = temp$navn)
-  
   coords <- tibble(lat = input$visueltcenter[[2]],
                    lon = input$visueltcenter[[1]])
   
+  # if (length(input$postnumre == 3)) {
+  #   
+  #   temp <- input$postnumre[[1]]
+  #   
+  #   zips <- tibble(zip_code = temp$nr,
+  #                  zip_name = temp$navn)
+  #   
+  # } else if (length(input$postnumre != 3)) {
+  #   
+  #   zips <- tibble(zip_code = NA,
+  #                  zip_name = NA)
+  #   
+  # }
+  
   df <- cbind(municipality,
-              zips,
               coords) %>% 
     tibble() %>% 
     mutate(city_name = name)
   
   return(df)
   
-  rm(name, municipality, temp, zips, coords, df)
+  rm(name, municipality, coords, df)
   
 }
 
+
+
+c1 <- cities[[1]]
+c2 <- cities[[2]]
+
+temp_list <- list(c1, c2)
 
 city_list <- map(cities,
                  extract_func)
 
 
+city_df <- bind_rows(city_list)
 
+city_df
