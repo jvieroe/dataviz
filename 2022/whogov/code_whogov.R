24520983486feabb574483a34a614682a2053066
jvieroe <jvieroe@gmail.com>
2022-03-19 18:47:43 +0100

    Update code_whogov.R

--- 2022/whogov/code_whogov.R
+++ 2022/whogov/code_whogov.R
@@ -3,18 +3,59 @@ library(sf)
 library(janitor)
 library(ggtext)
 
-# --------------- CREATE SEASON INDEX ---------------
-index <- tibble(
-  id = as.character(1:20),
-  season_long = paste0(2002:2021,
-                       "/",
-                       2003:2022),
-  season_short = paste0(str_pad(2:21,
-                                2,
-                                pad = "0"),
-                        "/",
-                        str_pad(3:22,
-                                2,
-                                pad = "0"))
-)
+# --------------- load WhoGov data ---------------
+# whogov_cs <- readxl::read_xlsx("2022/whogov/data/Denmark.xlsx")
+# 
+# whogov_ts <- readxl::read_xlsx("2022/whogov/data/Denmark_ts.xlsx")
+# 
+# 
+# whogov_cs <- whogov_cs %>% 
+#   select(name, birthyear, deadyear, placeofbirth)
+# 
+# whogov <- tidylog::left_join(whogov_ts,
+#                              whogov_cs,
+#                              by = )
+# 
+# 
+# rm(list=ls())
+
+
+whogov <- readxl::read_xlsx("2022/whogov/data/Denmark.xlsx")
+
+locations <- whogov %>% 
+  filter(!is.na(placeofbirth)) %>% 
+  pull(placeofbirth) %>% 
+  as.character()
+
+locations
+
+
+
+url <- "https://api.dataforsyningen.dk/supplerendebynavne2"
+
+library(httr)
+byer <- httr::GET(url)
+
+byer <- content(byer)
+
+b1 <- byer[[1]]
+
+b1$navn
+b1$kommune$kode
+b1$kommune$navn
+
+
+extract_func <- function(input, name, municipality, cooords) {
+  
+  name <- input$navn
+  
+  muni_code <- input$kommune$kode
+  muni_name <- input$kommune$navn
+  
+  
+  
+}
+
+
+
 
